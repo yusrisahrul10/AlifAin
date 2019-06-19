6742ded599de4cc6eaf10b7004e99388c749592e
Yusri <yxuxsxrxi@email.unikom.ac.id>
2019-06-19 09:35:52 +0700

    Auto stash before merge of "develop" and "origin/develop"

---
+++ app/src/main/java/com/example/alifain/category/NiqabActivity.kt
@@ -0,0 +1,13 @@
+package com.example.alifain.category
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.alifain.R
+
+class NiqabActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_niqab)
+    }
+}

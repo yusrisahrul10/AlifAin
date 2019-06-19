6947ab033846e4d012642bef7f13b6c2ea506fb6
Yusri <yxuxsxrxi@email.unikom.ac.id>
2019-06-20 00:00:12 +0700

    update buat layout checkout & detail barang

--- app/src/main/java/com/example/alifain/submain/CartFragment.kt
+++ app/src/main/java/com/example/alifain/submain/CartFragment.kt
@@ -1,22 +1,34 @@
 package com.example.alifain.submain
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.alifain.CheckoutActivity
 
 import com.example.alifain.R
 
 class CartFragment : Fragment() {
+
+    private lateinit var btnCheckout: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        val view = inflater.inflate(R.layout.fragment_cart, container, false)
+        btnCheckout = view.findViewById(R.id.btnCheckout)
+
+        btnCheckout.setOnClickListener {
+            val intent = Intent(context, CheckoutActivity::class.java)
+            startActivity(intent)
+        }
+        return view
     }
 
 
